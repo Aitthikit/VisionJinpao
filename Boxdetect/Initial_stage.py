dcb735419394392bd3ae83b0d7217ff06e719ec1
boannas <120669767+boannas@users.noreply.github.com>
2024-01-07 07:08:20 +0700

    Lasud

--- Boxdetect/Initial_stage.py
+++ Boxdetect/Initial_stage.py
@@ -3,43 +3,45 @@ import pyrealsense2 as rs
 import cv2
 import numpy as np
 import time
-import threading
-import queue
 
+# Configure depth and color streams
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)                
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 
+# Start streaming
+pipeline.start(config)
+start_time = time.time()
 
 try:
-    prev_time = time.time()
+    frame_count = 0
+    start_time = time.time()
     while True:
-
+        frame_count += 1
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        
+        if elapsed_time > 0:  # Avoid division by zero
+            fps = frame_count / elapsed_time
+            print(f"FPS: {fps}", end='\r')
+            
+        if elapsed_time > 1:
+            frame_count = 0
+            start_time = time.time()
             
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
+        
+        # Align the RGB and Depth frames
         align = rs.align(rs.stream.depth)
         aligned_frames = align.process(frames)
         
         # Extract the aligned RGB and Depth frames from the aligned frames
         depth_frame = aligned_frames.get_depth_frame()
         color_frame = aligned_frames.get_color_frame()
+        # color_frame = frames.get_color_frame()
         
-        # Get gyro frame
-        # gyro_frame = frames.first_or_default(rs.stream.gyro)
-        # if gyro_frame:
-        #     # Get gyroscope data
-        #     gyro_data = gyro_frame.as_motion_frame().get_motion_data()
-        #     # Calculate time delta
-        #     current_time = time.time()
-        #     elapsed_time = current_time - start_time
-        #     dt = current_time - prev_time
-        #     prev_time = current_time
-        #     # Calculate roll, pitch, and yaw
-        #     roll, pitch, yaw = ff.gyro_data_to_euler([gyro_data.x, gyro_data.y, gyro_data.z], dt)
-        #     c_r = -8.63/60 +0.28/60
-        #     c_p = 1.73/60 -0.64/60
-        #     c_y = -1.74/60 + 0.07/60
-        #     print("Roll: {:.2f}, Pitch: {:.2f}, Yaw: {:.2f}".format(np.degrees(roll) - (c_r*elapsed_time),np.degrees(pitch) - (c_p*elapsed_time),np.degrees(yaw) - (c_y*elapsed_time)))
-        
-
         if not depth_frame or not color_frame:
             continue
         
@@ -49,40 +51,36 @@ try:
         hsv = cv2.cvtColor(color_image,cv2.COLOR_BGR2HSV)
 
         # Create HSV range as numpy array
-        lower_green = np.array([65, 60, 40]) 
-        upper_green = np.array([75, 255, 255])
+        lower_green = np.array([70, 60, 60]) 
+        upper_green = np.array([85, 255, 255])
         
-        lower_red = np.array([0, 80, 55])
-        upper_red = np.array([10, 255, 255])
+        lower_red = np.array([170, 55, 50])
+        upper_red = np.array([180, 255, 255])
         
-        lower_blue = np.array([100, 70, 75])
-        upper_blue = np.array([130, 255, 255])
+        lower_blue = np.array([90, 60, 60])
+        upper_blue = np.array([110, 255, 255])
     
-        # create a mask for red color
+        # create a mask 
         mask_red = cv2.medianBlur(cv2.inRange(hsv, lower_red, upper_red),5)
-        # create a mask for green color
         mask_green = cv2.medianBlur(cv2.inRange(hsv, lower_green, upper_green),5)
-        # create a mask for blue color
-        mask_blue = cv2.medianBlur(cv2.inRange(hsv, lower_blue, upper_blue),5)
+        mask_blue = cv2.medianBlur(cv2.inRange(hsv, lower_blue, upper_blue),7)
     
-        # find contours in the red mask
-        contours_red, _ = cv2.findContours(cv2.medianBlur((mask_red),5), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # find contours in the green mask
-        contours_green, _ = cv2.findContours(cv2.medianBlur((mask_green),7), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # find contours in the blue mask
-        contours_blue, _ = cv2.findContours(cv2.medianBlur((mask_blue),5), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # find contours 
+        contours_red, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_green, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.04), cv2.COLORMAP_JET)
+        cv2.imshow("sss",depth_colormap)
         # Resolution of each frame
         color_res = ff.find_res(color_image)
         depth_res = ff.find_res(depth_colormap)
         
         # Show detected color with RGB 
-        # cv2.imshow("contour_gree",mask_green)
-        # cv2.imshow("contour_blue",mask_blue)
-        # cv2.imshow("contour_red",cv2.medianBlur((mask_red),9))
+        cv2.imshow("contour_gree",cv2.resize(mask_green,(mask_green.shape[1]//2,mask_green.shape[0]//2)))
+        cv2.imshow("contour_blue",cv2.resize(mask_blue,(mask_blue.shape[1]//2,mask_blue.shape[0]//2)))
+        cv2.imshow("contour_red",cv2.resize(mask_red,(mask_red.shape[1]//2,mask_red.shape[0]//2)))
         
         cv2.circle(color_image,(color_res[0]//2,color_res[1]//2),2,(0,0,255),5)         # Create a Origin circle
         for cnt in contours_red:
@@ -91,21 +89,24 @@ try:
                 x, y, w, h = cv2.boundingRect(cnt)
                 cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
                 cv2.circle(color_image,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
-                cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
+                # cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
                 if (w/h < 1.5) & (h/w < 1.5):
                     pos = ff.find_pos(color_image,w,x+int(w)//2 ,y+int(h)//2)
                     cv2.putText(color_image, "Box", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
                     # cv2.putText(color_image, "w: " + str(w) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)  
                     cv2.line(color_image,(x+int(w)//2 -80,y+int(h)//2),(x+int(w)//2+80 ,y+int(h)//2),(255,255,255),2)
                     cv2.line(color_image,(x+int(w)//2,y+int(h)//2-80),(x+int(w)//2 ,y+int(h)//2+80),(255,255,255),2)
-                    depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
+                    # depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
                     d = ff.find_Distance(w)
                     # cv2.putText(color_image, "d_RGB: " + str(d) + " mm", (x, y+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
-                    cv2.putText(color_image, "d_Sensor: " + str(depth_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
+                    # cv2.putText(color_image, "d_Sensor: " + str(dept h_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
                     cv2.putText(color_image, "x: " + str(round(pos[0],2)) + " mm ", (x, y+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
                     cv2.putText(color_image, "y: " + str(round(pos[1],2)) + " mm ", (x, y+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                elif(w/h>8) & (w/h<13):
+                    cv2.putText(color_image, "Strip " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                 else :
                     # cv2.putText(color_image, "Color Strip", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2) 
+                    cv2.putText(color_image, "XXX " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                     pass
             
 
@@ -116,23 +117,27 @@ try:
                 x, y, w, h = cv2.boundingRect(cnt)
                 cv2.rectangle(color_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.circle(color_image,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
-                cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
+                # cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
                 if (w/h < 1.5) & (h/w < 1.5):
                     pos = ff.find_pos(color_image,w,x+int(w)//2 ,y+int(h)//2)
                     cv2.putText(color_image, "Box", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
                     # cv2.putText(color_image, "d: " + str(w) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)  
-                    depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
+                    # depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
                     d = ff.find_Distance(w)
                     # cv2.putText(color_image, "d_RGB: " + str(d) + " mm", (x, y+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
                     # cv2.putText(color_image, "w: " + str(w) + " mm", (x, y+75), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
                     cv2.line(color_image,(x+int(w)//2 -80,y+int(h)//2),(x+int(w)//2+80 ,y+int(h)//2),(255,255,255),2)
                     cv2.line(color_image,(x+int(w)//2,y+int(h)//2-80),(x+int(w)//2 ,y+int(h)//2+80),(255,255,255),2)
-                    cv2.putText(color_image, "d_Sensor: " + str(depth_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
+                    # cv2.putText(color_image, "d_Sensor: " + str(depth_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
                     cv2.putText(color_image, "x: " + str(round(pos[0],2)) + " mm ", (x, y+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
                     cv2.putText(color_image, "y: " + str(round(pos[1],2)) + " mm ", (x, y+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                elif(w/h>8) & (w/h<13):
+                    cv2.putText(color_image, "Strip " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                 else :
                     # cv2.putText(color_image, "Color Strip", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2) 
+                    cv2.putText(color_image, "XXX " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                     pass
+                
         # loop through the blue contours and draw a rectangle around them
         for cnt in contours_blue:
             contour_area = cv2.contourArea(cnt)
@@ -141,28 +146,30 @@ try:
                 cv2.rectangle(color_image, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 # cv2.polylines(color_image,[cnt],True,(255,255,255),3)
                 cv2.circle(color_image,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
-                cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
+                # cv2.circle(depth_colormap,(x+int(w)//2 ,y+int(h)//2 ),2,(255,255,255),3)  
                 if (w/h < 1.5) & (h/w < 1.5):
                     pos = ff.find_pos(color_image,w,x+int(w)//2 ,y+int(h)//2)
                     cv2.putText(color_image, "Box", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
                     # cv2.putText(color_image, "d: " + str(w) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)  
-                    depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
-                    d = ff.find_Distance(w)
+                    # depth_value = depth_image[y+int(h)//2,x+int(w)//2  ]
                     # cv2.putText(color_image, "W: " + str(w) + " mm", (x, y+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
                     cv2.line(color_image,(x+int(w)//2 -80,y+int(h)//2),(x+int(w)//2+80 ,y+int(h)//2),(255,255,255),2)
                     cv2.line(color_image,(x+int(w)//2,y+int(h)//2-80),(x+int(w)//2 ,y+int(h)//2+80),(255,255,255),2)
-                    cv2.putText(color_image, "d_Sensor: " + str(depth_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
-                    cv2.putText(color_image, "x: " + str(round(pos[0],2)) + " mm ", (x, y+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-                    cv2.putText(color_image, "y: " + str(round(pos[1],2)) + " mm ", (x, y+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    # cv2.putText(color_image, "d_Sensor: " + str(depth_value) + " mm", (x, y+30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1) 
+                    # cv2.putText(color_image, "x: " + str(round(pos[0],2)) + " mm ", (x, y+45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                    # cv2.putText(color_image, "y: " + str(round(pos[1],2)) + " mm ", (x, y+70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+                elif(w/h>8) & (w/h<13):
+                    cv2.putText(color_image, "Strip " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                 else :
                     # cv2.putText(color_image, "Color Strip", (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2) 
+                    cv2.putText(color_image, "XXX " + str(w) + "  " + str(h), (x, y+10), cv2.FONT_HERSHEY_SIMPLEX,0.5, (255,255,255), 2)
                     pass
             # Create center in depth screen to reference        
-            cv2.circle(depth_colormap,(depth_res[0]//2, depth_res[1]//2),2,(255,255,255),3) 
+            # cv2.circle(depth_colormap,(depth_res[0]//2, depth_res[1]//2),2,(255,255,255),3) 
     
         # Rendering Screen of color and depth sensor of realsense
         cv2.imshow('RealSense', color_image)
-        cv2.imshow('Depth', depth_colormap)
+        # cv2.imshow('Depth', depth_colormap)
         cv2.waitKey(1)
 
 finally:
--- Boxdetect/my_Function.py
+++ Boxdetect/my_Function.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pyrealsense2 as rs
+import math
+import cv2
 known_length = 130
 known_height = 120
-focal_length = 650
-def find_Distance(pixel_lenght) :
-    distance = (known_length * focal_length) / pixel_lenght
-    return round(distance,2)
+# focal_length = 650
+# def find_Distance(pixel_lenght) :
+#     distance = (known_length * focal_length) / pixel_lenght
+#     return round(distance,2)
 
 def find_pos(image,w_h,x,y) :
     pixel_size_mm = w_h / known_height
@@ -28,11 +30,76 @@ def cameraFrame2cameraOrigin(position,theta):
          ,[0, 1, 0]
          ,[-np.sin(theta), 0, np.cos(theta)]]
     return np.matmul(R,position)
-roll, pitch, yaw = 0.0, 0.0, 0.0
-def gyro_data_to_euler(gyro_data, dt):
-    global roll, pitch, yaw
-    gx, gy, gz = gyro_data
-    roll += gx * dt
-    pitch += gy * dt
-    yaw += gz * dt
-    return roll, pitch, yaw
+
+def align(pipeline):
+    
+        # Wait for a new frame
+    frames = pipeline.wait_for_frames()
+    align = rs.align(rs.stream.depth)
+    aligned_frames = align.process(frames)
+    depth_frame = aligned_frames.get_depth_frame()
+    color_frame = aligned_frames.get_color_frame()
+
+    # if not depth_frame or not color_frame:
+    #     continue
+
+    depth_data = np.asanyarray(depth_frame.get_data())
+    color_data = np.asanyarray(color_frame.get_data())
+    return depth_data, color_data
+    
+def find_table(data, ans):
+    temp = [["","",""],["","",""],["","",""]]
+    # output = [["","",""],["","",""],["","",""]]
+    check = []
+    min_x = min_x = min(point[0] for point in data)
+    max_x = max(point[0] for point in data)
+    min_y = min(point[1] for point in data)
+    max_y = max(point[1] for point in data)
+
+    grid_x = ((max_x-min_x) / 3)+1
+    grid_y = ((max_y-min_y) / 3)+1
+    for i in data:
+        column = int((i[0]-min_x) // grid_x)
+        roll = int((i[1]-min_y) // grid_y)
+        temp[roll][column] = [i[3] , i[2]]
+        ans[roll][column] = str(i[3])
+
+    for i in range(len(temp)):
+        check.append("" not in temp[i])
+        # print("" in temp[i])
+    # print(check)
+    if all(check) == True:
+        # print(555) 
+        delta_x = max_x - min_x
+        d_left = (temp[0][0][1] + temp[1][0][1] + temp[2][0][1]) / 3
+        d_right = (temp[0][2][1] + temp[1][2][1] + temp[2][2][1]) / 3
+        delta_depth = d_right - d_left
+        angle_radians = math.atan2(delta_depth, delta_x)
+        angle_degrees = round(math.degrees(angle_radians),4)
+        return ans, angle_degrees
+    else :
+        # print(1230129370)
+        # return [["","",""],["","",""],["","",""]],0
+        # print(temp)
+        return ans, 99999
+    
+def create_ROI(min_distance, max_distance, color_data, depth_data):
+    # min_distance = 0.5  # in meters
+    # max_distance = 1.25  # in meters
+    depth_roi_mask = np.logical_and(depth_data >= min_distance * 1000, depth_data <= max_distance * 1000)
+    depth_roi = np.where(depth_roi_mask, depth_data, 0)
+    depth_roi_image = cv2.medianBlur(np.uint8(depth_roi / np.max(depth_roi) * 255),1)
+    contours, _ = cv2.findContours(depth_roi_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    new_frame = np.zeros_like(color_data)
+    cv2.drawContours(new_frame, contours, -1, (255, 255, 255), thickness=cv2.FILLED)
+    contour_area = cv2.bitwise_and(color_data, color_data, mask=new_frame[:, :, 0])
+    return contour_area
+
+# roll, pitch, yaw = 0.0, 0.0, 0.0
+# def gyro_data_to_euler(gyro_data, dt):
+#     global roll, pitch, yaw
+#     gx, gy, gz = gyro_data
+#     roll += gx * dt
+#     pitch += gy * dt
+#     yaw += gz * dt
+#     return roll, pitch, yaw
